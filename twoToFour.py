1ba2a2592011be2ccb63fbc2963bd8a7526c6fe9
oskarOlausson <kullenoskar@gmail.com>
2016-09-29 10:34:40 +0200

    n-size

--- twoToFour.py
+++ twoToFour.py
@@ -14,46 +14,48 @@ class Solver():
         w3 = 0.5
         w4 = 0.5
 
-        for i in range(0,5000):
+        for i in range(0,50000):
             number = 0
 
-            in1 = test[number+0]
-            in2 = test[number+1]
-            net1 = in1 * w1 + in2 * w2
-            net2 = in1 * w3 + in2 * w4
+            inp = [0] * 2
+            inp[0] = test[number+0]
+            inp[1] = test[number+1]
 
-            out1 = 1 / (1 + exp(-net1))
-            out2 = 1 / (1 + exp(-net2))
+            net = [0] * 2
+            net[0] = inp[0] * w1 + inp[1] * w2
+            net[1] = inp[0] * w3 + inp[1] * w4
 
-            facit = test[number+2]
+            out = [0] * 2
+            out[0] = 1 / (1 + exp(-net[0]))
+            out[1] = 1 / (1 + exp(-net[1]))
 
-            w1_to_out1 = (out1 - facit) * out1 * (1 - out1) * in1
+            #facit
+            facit = [0] * 2
+
+            facit[0] = test[number+2]
+            facit[1] = test[number+3]
+
+
+            w1_to_out1 = (out[0] - facit[0]) * out[0] * (1 - out[0]) * inp[0]
             w1 = w1-(w1_to_out1*learningRate)
-            print("w1 ",w1)
+            #print("w1 ",w1)
 
-            w2_to_out1 = (out1 - facit) * out1 * (1 - out1) * in2
+            w2_to_out1 = (out[0] - facit[0]) * out[0] * (1 - out[0]) * inp[1]
             w2 = w2 - (w2_to_out1 * learningRate)
-            print("w2 ", w2)
+            #print("w2 ", w2)
 
-            w3_to_out2 = (out2 - facit) * out2 * (1 - out2) * in1
+            w3_to_out2 = (out[1] - facit[1]) * out[1] * (1 - out[1]) * inp[0]
             w3 = w3 - (w3_to_out2 * learningRate)
-            print("w3 ", w3)
+            #print("w3 ", w3)
 
-            w4_to_out2 = (out2 - facit) * out2 * (1 - out2) * in2
+            w4_to_out2 = (out[1] - facit[1]) * out[1] * (1 - out[1]) * inp[1]
             w4 = w4 - (w4_to_out2 * learningRate)
-            print("w4 ", w4)
+            #print("w4 ", w4)
 
-            if out1>out2:
-                weSay = True
-                print("bigger")
-            else:
-                weSay = False
-                print("smaller")
 
-            correct = (weSay == facit)
+        print("w1: {}, w2: {}, w3: {}, w4: {}".format(w1, w2, w3, w4))
+        print("out1: {}, out2: {}, in1: {}, in2: {}".format(out[0], out[1], inp[0], inp[1]))
 
-            print(correct)
-            print()
 
 
 
@@ -61,7 +63,7 @@ if __name__ == "__main__":
     solver = Solver()
 
     # 3>2 --> True
-    test = [3,2,1]
+    test = [2, 0, 0.5, 0.2]
 
     solver.ai(test,3)
 
